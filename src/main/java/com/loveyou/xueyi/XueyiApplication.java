d0242ff0bb845a031ff8a51be2930c9f805e41fe
kim.jiang <115963077@qq.com>
2022-06-12 15:31:36 +0800

    Initial commit

---
+++ src/main/java/com/loveyou/xueyi/XueyiApplication.java
@@ -0,0 +1,13 @@
+package com.loveyou.xueyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XueyiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loveyou/xueyi/XueyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveyou.xueyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
