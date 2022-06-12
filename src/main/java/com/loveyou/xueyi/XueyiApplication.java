ef86dc493ab3b018d5fbd582368fe7ed2769613b
kim.jiang <115963077@qq.com>
2022-06-12 15:44:58 +0800

    We're waiting for you, yuexi.

--- src/main/java/com/loveyou/xueyi/XueyiApplication.java
+++ src/main/java/com/loveyou/xueyi/XueyiApplication.java
@@ -3,6 +3,9 @@ package com.loveyou.xueyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * We're waiting for you, Yuexi
+ */
 @SpringBootApplication
 public class XueyiApplication {
 
